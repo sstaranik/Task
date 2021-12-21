9cb55d14426b700210f3c47512e75baac89958a8
sstaranik <90226870+sstaranik@users.noreply.github.com>
2021-12-22 01:12:56 +0200

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,6 @@
+btn.addEventListener("Расчитать",f_out);
+
+   function f_out (){
+       E = ((Number(Q2.value) - Number(Q1.value)) / (Number(Q1.value)) / ((Number(P2.value) - Number(P1.value)) / (Number(P1.value));
+       res.innerHTML = E;
+   }
